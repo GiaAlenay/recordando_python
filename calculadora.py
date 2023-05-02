64d5b07b6d1832d1e990a57809c04d1cdc1bde22
Gianella Leon <giigl@hotmail.com>
2023-05-02 13:32:11 -0500

    todas las operaciones

--- calculadora.py
+++ calculadora.py
@@ -2,4 +2,20 @@
 
 n1 = input('Ingrese el primer numero: ')
 n2 = input('Ingrese el segundo número: ')
-print(n1, n2)
+
+n1 = int(n1)
+n2 = int(n2)
+
+suma = n1+n2
+resta = n1-n2
+multi = n1*n2
+div = n1/n2
+
+resultados = f"""
+Resultados de las operaciones entre {n1} y {n2}:
+La suma : {suma}
+La resta: {resta}
+La multiplicación: {multi}
+La divición: {div}
+"""
+print(resultados)
