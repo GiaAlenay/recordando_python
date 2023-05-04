03d1d8f507af49a8f824d3b6c7a3c69934398c25
Gianella Leon <giigl@hotmail.com>
2023-05-03 23:55:46 -0500

    trying

--- remove.py
+++ remove.py
@@ -1,14 +1,25 @@
 def frequenzi(word):
     contador = {}
     for i in word:
-        if contador[i]:
-            print('existe')
+        if i in contador:
+            contador[i] += 1
+
         else:
             contador[i] = 1
-            print('no existe')
 
-    # else:
-    #     return True
+    values = list(contador.values())
+    # print(valores)
+    min_freq = min(values)
+    max_freq = max(values)
+
+    if min_freq == max_freq:
+        return True
+    elif values.count(min_freq) == 1 and min_freq == 1:
+        return True
+    elif values.count(max_freq) == 1 and max_freq == min_freq + 1:
+        return True
+    else:
+        return False
 
 
-print(frequenzi('laa'))
+print(frequenzi('aassszz'))
