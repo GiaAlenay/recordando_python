b0ec6dabe1da097c240f7d95535a7bac893f8d80
Gianella Leon <giigl@hotmail.com>
2023-05-05 17:29:16 -0500

    ejer 12

--- manejoListas.py
+++ manejoListas.py
@@ -8,3 +8,16 @@ def Lista_Pares(lista):
 
 
 print(Lista_Pares([1, 2, 3, 4, 5, 6]))
+
+
+# Dada una lista de cadenas strings, escribe un programa que devuelva una nueva lista
+# que contenga solo las cadenas de longitud mayor o igual a 5.
+# Por ejemplo, si strings = ["hola", "python", "mundo", "hoy"],
+# el resultado debería ser ["python", "mundo"].
+
+def leng_mayor_5(lista_cadenas):
+    # return [li for li in lista_cadenas if len(li) > 4]
+    return list(filter(lambda li: len(li) > 4, lista_cadenas))
+
+
+print(leng_mayor_5(["hola", "python", "mundo", "hoy"]))
