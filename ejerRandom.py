d55bf0d5215b8805cc69b065c4aa87e34a23a77b
Gianella Leon <giigl@hotmail.com>
2023-05-15 22:55:01 -0500

    sumar pares ede una lista

--- ejerRandom.py
+++ ejerRandom.py
@@ -20,3 +20,10 @@ print(listaSinRep([1,1,2,3,5,8,2,10,'h']))
 def suma_entre(n1,n2):
     return sum(range(n1,n2+1))
 print(suma_entre(4,8))
+
+# Escribe una función que tome una lista de números como entrada y devuelva la suma de los números pares.
+
+def sumar_pares_lista(lista):
+    return sum([li for li in lista if li%2==0 ])
+
+print(sumar_pares_lista([46,5,8,7,6,1]))
