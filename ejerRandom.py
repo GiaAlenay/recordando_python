b8631249c227e21e7512b407c3458e657ea001f6
Gianella Leon <giigl@hotmail.com>
2023-05-15 22:47:43 -0500

    lista sin repetir

--- ejerRandom.py
+++ ejerRandom.py
@@ -4,3 +4,11 @@ def factorial(n):
         return 1
     return n * factorial(n-1)
 print(factorial(7))
+
+#Escribe una función que tome una lista como entrada y devuelva una nueva lista con los elementos únicos (sin duplicados).
+
+def listaSinRep(lista):
+
+    return list(set(lista))
+
+print(listaSinRep([1,1,2,3,5,8,2,10,'h']))
